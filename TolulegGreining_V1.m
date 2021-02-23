0dd781da0c3f99147284cabceafbb3dbeea97538
G0rocks <huldarh@gmail.com>
2021-02-23 12:42:08 +0000

    Liður 5, færa stoðlengda athuganir úr lið 6 yfir í lið 5. Bæta við kommentum og skýra útprentanir á niðurstöðum úr athugunum stoðlengda í lið 4 og 5

--- TolulegGreining_V1.m
+++ TolulegGreining_V1.m
@@ -133,7 +133,7 @@ xlabel('(a)');
 pbaspect([1 1 1])
 set(gca, 'box', 'off')
 
-[a_test_p1 a_test_p2 a_test_p3] = test_p(x,y,L1,L2,L3,x1,x2,y2,theta,gamma);
+[a_test_p1 a_test_p2 a_test_p3] = test_p(x,y,L1,L2,L3,x1,x2,y2,theta,gamma); % Prófa stoðlengdir og geyma
 
 %b) Teikna eina af fjórum stöðum á mynd b) í ramma 4
 %----------------------------------------------------%
@@ -148,8 +148,7 @@ xlabel('(b)');
 pbaspect([1 1 1])
 set(gca, 'box', 'off')
 
-[b_test_p1 b_test_p2 b_test_p3] = test_p(x,y,L1,L2,L3,x1,x2,y2,theta,gamma);
-
+[b_test_p1 b_test_p2 b_test_p3] = test_p(x,y,L1,L2,L3,x1,x2,y2,theta,gamma); % Prófa stoðlengdir og geyma
 
 
 % c) Teikna eina af fjórum stöðum á mynd c) í ramma 4
@@ -165,7 +164,7 @@ xlabel('(c)');
 pbaspect([1 1 1])
 set(gca, 'box', 'off')
 
-[c_test_p1 c_test_p2 c_test_p3] = test_p(x,y,L1,L2,L3,x1,x2,y2,theta,gamma);
+[c_test_p1 c_test_p2 c_test_p3] = test_p(x,y,L1,L2,L3,x1,x2,y2,theta,gamma); % Prófa stoðlengdir og geyma
 
  
 % d) Teikna eina af fjórum stöðum á mynd d) í ramma 4
@@ -181,22 +180,22 @@ xlabel('(d)');
 pbaspect([1 1 1])
 set(gca, 'box', 'off')
 
-% Check answers by verifying strut lengths
+[d_test_p1 d_test_p2 d_test_p3] = test_p(x,y,L1,L2,L3,x1,x2,y2,theta,gamma); % Prófa stoðlengdir og geyma
 
-[d_test_p1 d_test_p2 d_test_p3] = test_p(x,y,L1,L2,L3,x1,x2,y2,theta,gamma);
+% Check answers by verifying strut lengths
 
 disp('Athugum stoðlengdir, eigum að fá p1 = p2 = 5 og p3 = 3.');
 disp(' ');
-disp(strcat('Fyrir θ = ', num2str(raetur(1)), ' Faum vid:'))
+disp(strcat('a) Fyrir θ = ', num2str(raetur(1)), ' Faum vid:'))
 disp(strcat('p1 = ', num2str(a_test_p1), ', p2 = ', num2str(a_test_p2),' og p3 = ', num2str(a_test_p3)))
 disp(' ')
-disp(strcat('Fyrir θ = ', num2str(raetur(2)), ' Faum vid:'))
+disp(strcat('b) Fyrir θ = ', num2str(raetur(2)), ' Faum vid:'))
 disp(strcat('p1 = ', num2str(b_test_p1), ', p2 = ', num2str(b_test_p2), ' og p3 = ', num2str(b_test_p3)))
 disp(' ')
-disp(strcat('Fyrir θ = ', num2str(raetur(3)), ' Faum vid:'))
+disp(strcat('c) Fyrir θ = ', num2str(raetur(3)), ' Faum vid:'))
 disp(strcat('p1 = ', num2str(c_test_p1), ', p2 = ', num2str(c_test_p2), ' og p3 = ', num2str(c_test_p3)))
 disp(' ')
-disp(strcat('Fyrir θ = ', num2str(raetur(4)), ' Faum vid:'))
+disp(strcat('d) Fyrir θ = ', num2str(raetur(4)), ' Faum vid:'))
 disp(strcat('p1 = ', num2str(d_test_p1), ', p2 = ', num2str(d_test_p2), ' og p3 = ', num2str(d_test_p3)))
 disp(' ')
 disp('Allar lengdir stóðust!');
@@ -204,10 +203,11 @@ disp(' ');
 
 % Lidur 5
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Liður 4 aftur nema með aðra lengd fyrir stoð 2, p2
 disp(' ')
 disp('Lidur 5:')
 disp('--------------------------------')
-disp('(Sja myndir 5 og 6)')
+disp('(Sja myndir 5, plot af f, og 6, stöðurnar 6 sem uppfylla skilyrðin)')
 disp(' ')
 
 p2 = 7;
@@ -220,7 +220,7 @@ ezplot(f,[-pi,pi])
 % Skrifum litid rotaleitarforrit:
 %----------------------------------------------------%
 
-% Sjaum hvar raetur liggja af grafi
+% Sjaum hvar raetur liggja af grafi og veljum viðeigandi bil
 rotabil = [-1 -0.5;-0.38 -0.35;0 0.1;0.4 0.5;0.96 1;2.5 2.55];
 raetur5 = [];
 for k = 1:6
@@ -230,7 +230,7 @@ end
 %----------------------------------------------------%
 
 
-%a)
+%a) Teikna eina af sex stöðum á mynd a) í ramma 6
 %----------------------------------------------------%
 theta = raetur5(1);
 [f,x,y] = f_af_theta(p1,p2,p3,L1,L2,L3,x1,x2,y2,theta,gamma);
@@ -246,7 +246,7 @@ set(gca, 'box', 'off')
 
 [a_test_p1 a_test_p2 a_test_p3] = test_p(x,y,L1,L2,L3,x1,x2,y2,theta,gamma);
 
-% b)
+% b) Teikna eina af sex stöðum á mynd b) í ramma 6
 %----------------------------------------------------%
 subplot(3,2,2)
 theta = raetur5(2);
@@ -263,7 +263,7 @@ set(gca, 'box', 'off')
 
 
 
-% c)
+% c) Teikna eina af sex stöðum á mynd c) í ramma 6
 %----------------------------------------------------%
 subplot(3,2,3)
 theta = raetur5(3);
@@ -279,7 +279,7 @@ set(gca, 'box', 'off')
 [c_test_p1 c_test_p2 c_test_p3] = test_p(x,y,L1,L2,L3,x1,x2,y2,theta,gamma);
 
  
-% d)
+% d) Teikna eina af sex stöðum á mynd d) í ramma 6
 %----------------------------------------------------%
 subplot(3,2,4)
 theta = raetur5(4);
@@ -294,7 +294,7 @@ set(gca, 'box', 'off')
 
 [d_test_p1 d_test_p2 d_test_p3] = test_p(x,y,L1,L2,L3,x1,x2,y2,theta,gamma);
 
-% e)
+% e) Teikna eina af sex stöðum á mynd e) í ramma 6
 %----------------------------------------------------%
 subplot(3,2,5)
 theta = raetur5(5);
@@ -309,7 +309,7 @@ set(gca, 'box', 'off')
 
 [e_test_p1 e_test_p2 e_test_p3] = test_p(x,y,L1,L2,L3,x1,x2,y2,theta,gamma);
 
-% f)
+% f) Teikna eina af sex stöðum á mynd f) í ramma 6
 %----------------------------------------------------%
 subplot(3,2,6)
 theta = raetur5(6);
@@ -324,6 +324,31 @@ set(gca, 'box', 'off')
 
 [f_test_p1 f_test_p2 f_test_p3] = test_p(x,y,L1,L2,L3,x1,x2,y2,theta,gamma);
 
+% Check answers by verifying strut lengths
+
+disp('Athugum stoðlengdir, eigum að fá p1 = 5, p2 = 7 og p3 = 3.');
+disp(' ');
+disp(strcat('a) Fyrir θ = ', num2str(raetur5(1)), ' Faum vid:'))
+disp(strcat('p1 = ', num2str(a_test_p1), ', p2 = ', num2str(a_test_p2), ' og p3 = ', num2str(a_test_p3)))
+disp(' ')
+disp(strcat('b) Fyrir θ = ', num2str(raetur5(2)), ' Faum vid:'))
+disp(strcat('p1 = ', num2str(b_test_p1), ', p2 = ', num2str(b_test_p2), ' og p3 = ', num2str(b_test_p3)))
+disp(' ')
+disp(strcat('c) Fyrir θ = ', num2str(raetur5(3)), ' Faum vid:'))
+disp(strcat('p1 = ', num2str(c_test_p1), ', p2 = ', num2str(c_test_p2), ' og p3 = ', num2str(c_test_p3)))
+disp(' ')
+disp(strcat('d) Fyrir θ = ', num2str(raetur5(4)), ' Faum vid:'))
+disp(strcat('p1 = ', num2str(d_test_p1), ', p2 = ', num2str(d_test_p2), ' og p3 = ', num2str(d_test_p3)))
+disp(' ')
+disp(strcat('e) Fyrir θ = ', num2str(raetur5(5)), ' Faum vid:'))
+disp(strcat('p1 = ', num2str(e_test_p1), ', p2 = ' + num2str(e_test_p2), ' og p3 = ', num2str(e_test_p3)))
+disp(' ')
+disp(strcat('f) Fyrir θ = ', num2str(raetur5(6)), ' Faum vid:'))
+disp(strcat('p1 = ', num2str(f_test_p1), ', p2 = ', num2str(f_test_p2), ' og p3 = ', num2str(f_test_p3)))
+
+disp('Allar lengdir stóðust!');
+disp(' ');
+
 
 % Lidur 6
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -333,21 +358,4 @@ disp('Lidur 6:')
 disp('--------------------------------')
 disp(' ')
 
-disp(strcat('Fyrir θ = ', num2str(raetur5(1)), ' Faum vid:'))
-disp(strcat('p1 = ', num2str(a_test_p1) + ', p2 = ', num2str(a_test_p2), ' og p3 = ', num2str(a_test_p3)))
-disp(' ')
-disp(strcat('Fyrir θ = ', num2str(raetur5(2)), ' Faum vid:'))
-disp(strcat('p1 = ', num2str(b_test_p1) + ', p2 = ', num2str(b_test_p2), ' og p3 = ', num2str(b_test_p3)))
-disp(' ')
-disp(strcat('Fyrir θ = ', num2str(raetur5(3)), ' Faum vid:'))
-disp(strcat('p1 = ', num2str(c_test_p1), ', p2 = ', num2str(c_test_p2), ' og p3 = ', num2str(c_test_p3)))
-disp(' ')
-disp(strcat('Fyrir θ = ', num2str(raetur5(4)), ' Faum vid:'))
-disp(strcat('p1 = ', num2str(d_test_p1), ', p2 = ', num2str(d_test_p2), ' og p3 = ', num2str(d_test_p3)))
-disp(' ')
-disp(strcat('Fyrir θ = ', num2str(raetur5(5)), ' Faum vid:'))
-disp(strcat('p1 = ', num2str(e_test_p1), ', p2 = ' + num2str(e_test_p2), ' og p3 = ', num2str(e_test_p3)))
-disp(' ')
-disp(strcat('Fyrir θ = ', num2str(raetur5(6)), ' Faum vid:'))
-disp(strcat('p1 = ', num2str(f_test_p1), ', p2 = ', num2str(f_test_p2), ' og p3 = ', num2str(f_test_p3)))
 
