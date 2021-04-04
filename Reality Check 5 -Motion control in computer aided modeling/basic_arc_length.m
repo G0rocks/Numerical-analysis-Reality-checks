1eb7be95bb4478f9618804c385df6e1ba997574a
Huldar <huldarh@gmail.com>
2021-04-04 11:23:48 +0000

    Add function
    Computes the arc length for a given 1st x and y derivative and left and right boundaries according to the formula from calculus I

---
+++ Reality Check 5 -Motion control in computer aided modeling/basic_arc_length.m
@@ -0,0 +1,10 @@
+% Function for computing the basic arc length for given
+% First x derivative, x_der1
+% First y derivative, y_der1
+% Left boundary, a
+% Right boundary, b
+
+function lengd = basic_arc_length(x_der1_local, y_der1_local, a, b)
+  fun = @(t) pythagoras_C(x_der1_local(t), y_der1_local(t)); % creating function to integrate
+  lengd = integral(fun, a, b);  % Integrate to find arc length
+end % Function
