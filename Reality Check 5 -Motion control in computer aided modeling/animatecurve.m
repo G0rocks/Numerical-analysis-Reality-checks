7f49eef67fb1fb99acb01c89832ce1d99cf3285f
ragnartomasson <ragnartomasson@localhost>
2021-04-09 14:43:15 +0000

    lagar lid7, gerir gif

--- Reality Check 5 -Motion control in computer aided modeling/animatecurve.m
+++ Reality Check 5 -Motion control in computer aided modeling/animatecurve.m
@@ -3,23 +3,32 @@
 %s er s
 %k er numer a mynd
 
-function animatecurve(xf,yf,s,k)
+function animatecurve(xf,yf,a,b,h,k)
 figure(k)
 cla
-n=linspace(0,s,10000);
-
+n=linspace(a,b,abs(b-a)*10000);
 hold on
-for i=0:0.01:s
+filename=strcat('figure ', k,'.gif')
+for i=a:h:b
     x=xf(i);
     y=yf(i);
-    
     %set(gca,'XLim',[0 1],'YLim',[0 1],'Drawmode','fast', 'Visible','on');
     set(gca,'XLim',[0 1],'YLim',[0 1], 'SortMethod', 'childorder', 'Visible','on');
     axis square
     cla
     plot(xf(n),yf(n))
     ball = line('color','r','Marker','o','MarkerSize',10, 'LineWidth',2,'xdata',[],'ydata',[]);
-
-    
     set(ball,'xdata',x,'ydata',y);drawnow;pause(0.01)
+          % Capture the plot as an image 
+      frame = getframe(figure(k)); 
+      im = frame2im(frame); 
+      [imind,cm] = rgb2ind(im,256); 
+      % Write to the GIF File 
+      if i == a
+          imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
+      else 
+          imwrite(imind,cm,filename,'gif','WriteMode','append'); 
+      end 
 end
+
+
